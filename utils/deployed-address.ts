fb3a1a2af626272a84d9f62cfcf209b6df0775fb
huniol w <passaupo21@gmail.com>
2024-08-16 06:10:20 +0000

    feat: initiated the project

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xC8Aee92A85506C22DD659e7FFf637Ea7bF91e355'
+export const ProxyAdmin = '0xc07297536efaB0Bc0B4667a961EBa0ff86cf9a2F'
+export const SWTRImplementation = '0x8b465124E95F3AF25479d33CB5DE2e3A45221f9c'
     
